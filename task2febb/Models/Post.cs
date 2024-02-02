63c24fc445cff9c04b1bb8014d1a66ebe4495aaa
john <john123@gmail.com>
2024-02-02 16:34:24 +0530

    Add project files.

---
+++ task2febb/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace task2febb.Models
+{
+    [Table("Post")]
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Author { get; set; }
+    }
+}
